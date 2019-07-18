e1a4dffc0b4ae2791f8c5ad0848178632af11058
Omar Polo <omar.polo@europecom.net>
2019-07-18 11:49:04 +0200

    removed unnecessary indent comment directive

--- include/fmt.h
+++ include/fmt.h
@@ -3,12 +3,8 @@
 
 #include <stdarg.h>
 #include <stddef.h>
-/* *INDENT-OFF* */
-/* indent wrong indent the following line */
 
-typedef size_t (*writefn) (void *, size_t);
-
-/* *INDENT-ON* */
+typedef size_t(*writefn) (void *, size_t);
 
 void fmt(writefn, char *, ...);
 void vafmt(writefn, char *, va_list);
