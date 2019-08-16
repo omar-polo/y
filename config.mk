04753af5ac844bb55276efc862e9557fc4ebe673
Omar Polo <omar.polo@europecom.net>
2019-08-16 15:19:55 +0200

    define nil=NULL

--- config.mk
+++ config.mk
@@ -4,5 +4,6 @@ AR	 = ${TARGET}-ar
 LD	 = ${TARGET}-ld
 OBJCOPY	 = ${TARGET}-objcopy
 MAKE = make
+BOCHS= bochs
 
-CFLAGS	 = -nostdlib -std=c99 -ffreestanding -O2 -Wall -Wextra -I${MAINDIR}/include -I${MAINDIR}
+CFLAGS	 = -nostdlib -std=c99 -ffreestanding -O2 -Wall -Wextra -I${MAINDIR}/include -I${MAINDIR} -Dnil=NULL
